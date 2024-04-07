4b38c498b48b394a944d8e2894933b2467ffe60a
Jigar Lodaya <jigar.lodaya@riyalto.com>
2024-04-07 14:52:39 +0530

    Setup DB Connection

--- src/server.js
+++ src/server.js
@@ -1,40 +1,9 @@
-import express from "express";
-const app = express();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 const port = process.env.PORT || 3000;
 
-app.get("/api/jokes", (req, res) => {
-  res.json([
-    {
-      color: "red",
-      value: "#f00",
-    },
-    {
-      color: "green",
-      value: "#0f0",
-    },
-    {
-      color: "blue",
-      value: "#00f",
-    },
-    {
-      color: "cyan",
-      value: "#0ff",
-    },
-    {
-      color: "magenta",
-      value: "#f0f",
-    },
-    {
-      color: "yellow",
-      value: "#ff0",
-    },
-    {
-      color: "black",
-      value: "#000",
-    },
-  ]);
+dotenv.config({
+  path: "./env",
 });
 
-app.listen(port, () => {
-  console.log(`Goal tracker backend listening on port ${port}`);
-});
+connectDB();
